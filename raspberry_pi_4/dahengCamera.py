1d1f2753a95b5dfe88308ec37420c7dac26c52cd
TIng Sheng Lee <lee@a-i.land>
2023-03-17 07:57:16 +0100

    version: 1.0.0

--- raspberry_pi_4/dahengCamera.py
+++ raspberry_pi_4/dahengCamera.py
@@ -55,8 +55,10 @@ while True:
 			cams[0].LineSelector.set(1)
 			cams[0].LineMode.set(1)
 			cams[0].LineInverter.set(False)
-			cams[0].LineSource.set(5)
-			cams[0].SensorShutterMode.set(2)
+			cams[0].LineSource.set(2)
+			#cams[0].SensorShutterMode.set(2)
+			cams[0].UserOutputSelector.set(1)
+			#cams[0].UserOutputMode.set(1)
 			
 			cam_stream_on(cams)
 
@@ -68,7 +70,7 @@ while True:
 			cam_inited = True
 			
 		except:
-			
+			print(traceback.format_exc())
 			print(loop_t, ": daheng camera failed to initialized")
 			cam_inited = False
 			
@@ -82,29 +84,49 @@ while True:
 			
 			print(loop_t + ": Starting calibration for daheng")
 			
+			cams[0].UserOutputValue.set(True)
 			cams[0].GainAuto.set(2)
 			cams[0].BalanceWhiteAuto.set(2)
 						
-			while True:
-		
-				cams[0].TriggerSoftware.send_command()	
-		
-				if cams[0].GainAuto.get()[0] == 0 and cams[0].BalanceWhiteAuto.get()[0] == 0:
-					sql_inited = set_value(mydb, sql_inited, 'control', 'do_calibration', '0')
-					print(loop_t + ": Daheng calibration completed")
-					break		
+			for i in range(3):
+
+				while True:
 			
+					cams[0].TriggerSoftware.send_command()
+					images = get_numpyImageBGR(cams)	
+					
+					if cams[0].GainAuto.get()[0] == 0 and cams[0].BalanceWhiteAuto.get()[0] == 0:
+						cams[0].UserOutputValue.set(False)
+						sql_inited = set_value(mydb, sql_inited, 'control', 'do_calibration', '0')
+						print(loop_t,  ": Daheng calibration completed")
+						
+						# get required information for logging
+						cam_gain = cams[0].Gain.get()
+						cams[0].BalanceRatioSelector.set(0)
+						cam_red = cams[0].BalanceRatio.get()
+						cams[0].BalanceRatioSelector.set(1)
+						cam_green = cams[0].BalanceRatio.get()
+						cams[0].BalanceRatioSelector.set(2)
+						cam_blue = cams[0].BalanceRatio.get()
+						
+						print("Gain:", cam_gain, "White Balance:", cam_red, cam_green, cam_blue)
+						
+						break		
+		
 		# Manually set daheng camera
 		if daheng[4]:
 			
 			cams[0].ExposureTime.set(daheng[0])
 			sql_inited = set_value(mydb, sql_inited, 'daheng_camera', 'make_change', '0')
 			print(loop_t + ": Manual changes on daheng camera completed")
+			print("Exposure: ", cams[0].ExposureTime.get())
 									
 		# Capture image and preview it
 		if cam_inited and debug:
 			try:
+							
 				cams[0].TriggerSoftware.send_command()	
+				cams[0].UserOutputValue.set(False)
 				images = get_numpyImageBGR(cams)
 				
 				cv2.imshow("preview", images[0])
@@ -119,19 +141,75 @@ while True:
 					print(traceback.format_exc())
 		 
 		# Save the image via ftp
-		if ftp_enable and ftp_inited and control[0] and not daheng[3]:
-			cams[0].TriggerSoftware.send_command()	
+		if cam_inited and ftp_enable and ftp_inited and control[0] and not daheng[3]:
+			
+			print(loop_t + ": Starting calibration for daheng")
+				
+			cams[0].UserOutputValue.set(True)
+			cams[0].GainAuto.set(2)
+			cams[0].BalanceWhiteAuto.set(2)
+						
+			for i in range(3):
+
+				while True:
+			
+					cams[0].TriggerSoftware.send_command()
+					images = get_numpyImageBGR(cams)	
+					
+					if cams[0].GainAuto.get()[0] == 0 and cams[0].BalanceWhiteAuto.get()[0] == 0:
+						cams[0].UserOutputValue.set(False)
+						sql_inited = set_value(mydb, sql_inited, 'control', 'do_calibration', '0')
+						print(loop_t,  ": Daheng calibration completed")
+						
+						# get required information for logging
+						cam_gain = cams[0].Gain.get()
+						cams[0].BalanceRatioSelector.set(0)
+						cam_red = cams[0].BalanceRatio.get()
+						cams[0].BalanceRatioSelector.set(1)
+						cam_green = cams[0].BalanceRatio.get()
+						cams[0].BalanceRatioSelector.set(2)
+						cam_blue = cams[0].BalanceRatio.get()
+						
+						print("Gain:", cam_gain, "White Balance:", cam_red, cam_green, cam_blue)
+						
+						break	
+							
+			cams[0].UserOutputValue.set(True)
+			cams[0].TriggerSoftware.send_command()
 			images = get_numpyImageBGR(cams)
+			cams[0].UserOutputValue.set(False)
+			
+			# Crop image border
+			im_size = images[0].shape
+			left_crop = 500
+			right_crop = 500
+			top_crop = 58
+			bottom_crop = 58
+			
+			images[0] = images[0][top_crop:im_size[0]-bottom_crop,left_crop:im_size[1]-right_crop]
+
 			retval_1, buffer_1 = cv2.imencode(".jpg", images[0])
 			flo1 = BytesIO(buffer_1)
 			
 			sql_inited, image_name = get_value(mydb, sql_inited, 'control', 'image_name')
-			print(image_name)
 			
+			# camera logging info
+			cam_expo = cams[0].ExposureTime.get()
+			cam_gain = cams[0].Gain.get()
+			cams[0].BalanceRatioSelector.set(0)
+			cam_red = cams[0].BalanceRatio.get()
+			cams[0].BalanceRatioSelector.set(1)
+			cam_green = cams[0].BalanceRatio.get()
+			cams[0].BalanceRatioSelector.set(2)
+			cam_blue = cams[0].BalanceRatio.get()
+			log_info = 'Exposure: ' + str(cam_expo) + '; Gain: ' + str(cam_gain) + '; WB: ' + str(cam_red) + ', ' + str(cam_green) + ', ' + str(cam_blue)
+
 			try:
 				ftp.storbinary('STOR daheng/' + image_name, flo1)		
 				sql_inited = set_value(mydb, sql_inited, 'daheng_camera', 'capture', '1')
-				print(loop_t, ": Done saving daheng image via ftp")
+				sql_inited = set_value(mydb, sql_inited, 'daheng_camera', 'log_info', '"' + log_info + '"')
+				print(loop_t, ": Done saving daheng image via ftp as", image_name)
+				print(log_info)
 			
 			except:
 			
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Setup
+1. clone the repository with the following command
+```
+git clone https://github.com/A-I-Land/handwagon.git
+```
+2. put the *startArducam.sh* file in the root directory, e.g. /home/ailand
+3.  
+
+# Troubleshooting
